1780b1ac8d8828a87e0aea936aa9a43692711093
Alexander Tukai <tukai@alutech-mc.com>
2016-07-14 16:13:48 +0300

    update to psr-4

--- src/Module.php
+++ src/Module.php
@@ -2,7 +2,6 @@
 
 namespace AtAdmin;
 
-use Zend\EventManager\EventInterface;
 use Zend\Mvc\MvcEvent;
 use Zend\Mvc\Router\RouteMatch;
 
@@ -11,17 +10,6 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
     }
 
     /**
